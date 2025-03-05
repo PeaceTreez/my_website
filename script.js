9cc3f9712fb698e9efb4f9e0b2d7447ca7522f88
Mzi <70058726+PeaceTreez@users.noreply.github.com>
2025-03-05 21:55:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+
+// Add event listener to navigation links
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll("header nav a");
+
+    navLinks.forEach((link) => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute("href").slice(1);
+            const targetSection = document.getElementById(targetId);
+            targetSection.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+});
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.getElementById('nav-links').classList.toggle('show');
+});
