c1d0be1bfe43a8407b5ea1038bcd3abbff1420bd
PeaceTreez <70058726+PeaceTreez@users.noreply.github.com>
2026-02-10 17:06:44 +0200

    Update professional portfolio

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // script.js
 
 // Add event listener to navigation links
@@ -16,4 +17,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.getElementById('menu-toggle').addEventListener('click', function() {
     document.getElementById('nav-links').classList.toggle('show');
+=======
+// script.js
+
+// Add event listener to navigation links
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll("header nav a");
+
+    navLinks.forEach((link) => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute("href").slice(1);
+            const targetSection = document.getElementById(targetId);
+            targetSection.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+});
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.getElementById('nav-links').classList.toggle('show');
+>>>>>>> 5ae2a38 (Initial commit: Professional portfolio update.)
 });
